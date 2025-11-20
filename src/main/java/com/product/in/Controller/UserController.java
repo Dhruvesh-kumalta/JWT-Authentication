a38549e505f21606535c9e22b93d60258289dd18
Dhruvesh-kumalta <dhruvsinghkumalta@gmail.com>
2025-11-20 19:49:30 +0530

    Adding more feature for update,delete,read all users

--- src/main/java/com/product/in/Controller/UserController.java
+++ src/main/java/com/product/in/Controller/UserController.java
@@ -3,14 +3,19 @@ package com.product.in.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.in.Sevice.UserService;
 import com.product.in.entity.User_Data;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/user")
@@ -29,5 +34,17 @@ public class UserController {
         return userService.getUsersByUsername();
     }
     
+    @GetMapping("/")
+    public List<User_Data> getMethodName() {
+        return userService.getUsersByUsername();
+    }
+    @PutMapping("path/{id}")
+    public User_Data putMethodName(@PathVariable Long id, @RequestBody User_Data entity) {
+          return userService.updateUser(id, entity);
+    }
+    @DeleteMapping("/")
+    public String deleteUser(@PathVariable Long id) {
+    	return userService.deleteUser(id);
+    }
     
 }
